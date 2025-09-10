20cb686de795151f764e4d62e99ab62ccdbc34eb
Daniel Duque <duque@outlook.com>
2025-09-10 11:23:26 -0600

    Comment out unused image import and variable in Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import NavLink from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 import { Link } from "react-scroll";
 import { ISocialLink } from "@/types/types";
 
-const himg = "/images/headshot.png";
+// const himg = "/images/headshot.png";
 
 /**
  * Social media links configuration
